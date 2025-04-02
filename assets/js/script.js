5d8faac68926d88714f55b6faf597e75497266c5
Jennifer02623 <jennifermanoppo1165@gmail.com>
2025-04-03 02:42:01 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".teks", {
+  strings: ["Matakuliah Bioinformatika", "Dosen : NANCY JEANE TUTUROONG ST, MT"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 1000,
+  loop: true,
+});
