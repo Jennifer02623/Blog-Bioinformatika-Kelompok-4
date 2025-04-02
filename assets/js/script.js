9533786afcf6e38a2ea7167d7f72587c5aa85d7f
Jennifer02623 <jennifermanoppo1165@gmail.com>
2025-04-03 03:05:09 +0800

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-let typed = new Typed(".teks", {
-  strings: ["Matakuliah Bioinformatika", "Dosen : NANCY JEANE TUTUROONG ST, MT"],
-  typeSpeed: 60,
-  backSpeed: 60,
-  backDelay: 1000,
-  loop: true,
-});
